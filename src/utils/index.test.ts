41481c3ef7e36e242ee9bdeab261d1ea62aefa5b
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2023-04-30 18:00:04 +0900

    feat: Utility function for extracting script as string

---
+++ src/utils/index.test.ts
@@ -0,0 +1,59 @@
+import { extractScriptAsString, deleteNamespace } from './';
+
+const scriptAsString = `
+  let x = 0;
+
+  const incrementX = () => {
+    ++x;
+  };
+
+  const getX = () => {
+    return x;
+  };
+`;
+
+const scriptContainerString = `export const scriptContainer = () => {
+${scriptAsString}
+};`;
+
+const scriptWithNamespace = `
+  const graphicequalizer = new __NAMESPACE__.GraphicEqualizer(window.X.get());
+
+  const f0 = __NAMESPACE__.FREQUENCIES[0];
+
+  graphicequalizer.param(__NAMESPACE__.FREQUENCIES[1], f0);
+
+  Object.keys(__NAMESPACE__.PRESETS).forEach((preset) => {
+  });
+
+  const canvas  = document.createEleemnt('canvas');
+  const context = canvas.getContext('2d');
+  const knob    = new __NAMESPACE__.Knob(canvas, context);
+`;
+
+const scriptWithoutNamespace = `
+  const graphicequalizer = new GraphicEqualizer(window.X.get());
+
+  const f0 = FREQUENCIES[0];
+
+  graphicequalizer.param(FREQUENCIES[1], f0);
+
+  Object.keys(PRESETS).forEach((preset) => {
+  });
+
+  const canvas  = document.createEleemnt('canvas');
+  const context = canvas.getContext('2d');
+  const knob    = new Knob(canvas, context);
+`;
+
+describe(`${extractScriptAsString.name}`, () => {
+  test('should return executable script as string', () => {
+    expect(extractScriptAsString(scriptContainerString)).toBe(`\n${scriptAsString}\n`);
+  });
+});
+
+describe(`${deleteNamespace.name}`, () => {
+  test('should return script that does not have namespace', () => {
+    expect(deleteNamespace(scriptWithNamespace)).toBe(scriptWithoutNamespace);
+  });
+});
---
+++ src/utils/index.ts
@@ -0,0 +1,21 @@
+/**
+ * This function returns executable script as string by removing container string.
+ * @param {string} scriptContainerString This argument is string that contains executable script.
+ * @return {string} Return value is executable script as string.
+ */
+export function extractScriptAsString(scriptContainerString: string): string {
+  return scriptContainerString.replace(/^.+?\{/s, '').replace(/\};?$/s, '');
+}
+
+/**
+ * This function deletes namespaces that are attached on bundling.
+ * @param {string} scriptString This argument is executable script.
+ * @return {string} Return value is script that does not have namespace.
+ */
+export function deleteNamespace(scriptString: string): string {
+  return scriptString
+    .replace(/(.*= new) .*GraphicEqualizer/g, '$1 GraphicEqualizer')
+    .replace(/(.*?)_.*\.FREQUENCIES(.*)/g, '$1FREQUENCIES$2')
+    .replace(/(.*?)_.*\.PRESETS(.*)/g, '$1PRESETS$2')
+    .replace(/(.*= new) .*Knob/g, '$1 Knob');
+};
