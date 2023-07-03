27a55afd68c2be3c8c65aa719f7f087dc6f246d6
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2023-07-03 22:00:37 +0900

    chore: Add config for using absolute import

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
-  preset         : 'ts-jest',
-  testEnvironment: 'jest-environment-jsdom'
+  preset          : 'ts-jest',
+  testEnvironment : 'jest-environment-jsdom',
+  moduleNameMapper: {
+    '^/src/(.*)$': '<rootDir>/src/$1'
+  }
 };
